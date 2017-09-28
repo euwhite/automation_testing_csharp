19634adf105e2189de7e1dcba817b1cd291d63e5
Eugene White <e.white1937@gmail.com>
2017-09-29 00:36:50 +0300

    "Hours" exercise

---
+++ UltimateQALessons/ReferenceTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //MyClass a = new MyClass();
+            //a.Value = "start Value";
+
+            //MyClass b = a;
+            //b.Value = "new Value";
+
+            //Console.WriteLine(a.Value);
+            //Console.ReadLine();
+
+            int a = 1;
+            int b = a;
+
+            b = 2;
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+
+    class MyClass
+    {
+        public string Value { get; set; }
+    }
+}
